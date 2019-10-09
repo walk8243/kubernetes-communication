3e6050287e43624c6c63e3afccb2e2bc8f60802f
walk8243 <walk8243@gmail.com>
2019-10-10 01:04:22 +0900

    検証環境を作成する実行スクリプトを作成

--- deploy.cmd
+++ deploy.cmd
@@ -1,8 +1,24 @@
-minikube start
+@echo off
+setlocal
 
-kubectl create namespace k8s-communication
-kubectl create namespace k8s-communication-sub
+REM Startup confirmation minikube
+set TMP_FILE1=minikube_status.log
+set TMP_FILE2=minikube_host_status.log
+minikube status > %TMP_FILE1%
+find /i "host:" %TMP_FILE1% > %TMP_FILE2%
+find /i "Running" %TMP_FILE2% > nul
+if not %ERRORLEVEL% equ 0 (
+  minikube start
+)
+if exist %TMP_FILE1% ( del %TMP_FILE1% )
+if exist %TMP_FILE2% ( del %TMP_FILE2% )
 
-kubectl create -f deploy.yaml
-kubectl expose deployment server --type=LoadBalancer --port=80
-minikube service server
+REM deploy for minikube
+kubectl create namespace k8s-communication > nul 2>&1
+kubectl create namespace k8s-communication-sub > nul 2>&1
+kubectl apply -f deploy.yaml > nul 2>&1
+kubectl expose deployment server --type=LoadBalancer --port=80 --namespace=k8s-communication > nul 2>&1
+minikube service server --namespace=k8s-communication
+
+endlocal
+exit /b 0
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-minikube start
+# Startup confirmation minikube
+MINIKUBE_STATUS=`minikube status | grep "host:" | awk -F':' '{print $2}'`
+if [ $MINIKUBE_STATUS != "Running" ]; then
+  minikube start
+fi
 
-kubectl create namespace k8s-communication
-kubectl create namespace k8s-communication-sub
-
-kubectl create -f deploy.yaml
-kubectl expose deployment server --type=LoadBalancer --port=80
-minikube service server
+# deploy for minikube
+kubectl create namespace k8s-communication > /dev/null 2>&1
+kubectl create namespace k8s-communication-sub > /dev/null 2>&1
+kubectl apply -f deploy.yaml > /dev/null 2>&1
+kubectl expose deployment server --type=LoadBalancer --port=80 --namespace=k8s-communication > /dev/null 2>&1
+minikube service server --namespace=k8s-communication
